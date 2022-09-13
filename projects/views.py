9c2c6b0315b120cc2c1cc13b03b9f9c2bed562db
esteban <esteban.chirinos@gmail.com>
2022-09-13 13:08:50 -0400

    Feature 13 complete

--- projects/views.py
+++ projects/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from projects.models import Project
 from django.contrib.auth.mixins import LoginRequiredMixin
 
@@ -7,6 +7,14 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 class ProjectListView(LoginRequiredMixin, ListView):
     model = Project
     template_name = "projects/list.html"
+
+    def get_queryset(self):
+        return Project.objects.filter(members=self.request.user)
+
+
+class ProjectDetailView(LoginRequiredMixin, DetailView):
+    model = Project
+    template_name = "projects/detail.html"
 
     def get_queryset(self):
         return Project.objects.filter(members=self.request.user)
